37614f2c6a6f19995761b6048231c802212ca448
shawnth13 <shawnth@uci.edu>
2022-04-19 15:25:59 -0700

    Temp Candle Light
    Added the renderer and candle light. Needs some adjustment but it grows smaller as you move.

---
+++ IntoDarknessGame2D/Assets/Scripts/CandleLight.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class CandleLight : MonoBehaviour
+{
+    private GameObject Player;
+    private Rigidbody2D playerRb;
+    private Light2D light2d;
+    
+    [SerializeField]
+    public enum diminishTypeEnum{Constant, Lerp};
+    public diminishTypeEnum diminishType;
+    [SerializeField]
+    public float diminishValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = gameObject.transform.parent.gameObject;
+        playerRb = Player.GetComponent<Rigidbody2D>();
+        light2d = GetComponent<Light2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerRb.velocity.x != 0 || playerRb.velocity.y != 0){
+            switch(diminishType){
+                case diminishTypeEnum.Constant:
+                    light2d.pointLightInnerRadius -= diminishValue / 1000;
+                    light2d.pointLightOuterRadius -= diminishValue / 1000;
+                    break;
+                case diminishTypeEnum.Lerp:
+                    light2d.pointLightInnerRadius *= diminishValue;
+                    light2d.pointLightOuterRadius *= diminishValue;
+                    break;
+            }
+        }
+    }
+}
