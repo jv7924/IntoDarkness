868c2742398cb181a22bc02acf222fdc7caf88be
Jose <78896687+jv7924@users.noreply.github.com>
2022-05-20 21:05:46 -0700

    Fixed Moving Platform
    Finally fixed the moving platform so now it works with the button.

---
+++ IntoDarknessGame2D/Assets/Scripts/PlatformMover.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMover : MonoBehaviour
+{
+    // Empty objects to use as points of travel
+    public GameObject objStartPos;
+    public GameObject objEndPos;
+
+    // Speed of moving platform
+    public float platSpeed = 3f;
+
+    // Positions that platform should start and end on 
+    private Vector2 startPos;
+    private Vector2 endPos;
+    private bool buttonClicked = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Gets the start and end position from empty objects in game
+        startPos = (Vector2)objStartPos.transform.position;
+        endPos = (Vector2)objEndPos.transform.position;
+
+        // Sets the platforms starting position to that of the startPos object
+        if ((Vector2)transform.position != startPos)
+        {
+            transform.position = startPos;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if ((Vector2)transform.position == endPos && buttonClicked)
+        {
+            Debug.Log("In if");
+            StartCoroutine(LerpPosition(gameObject, startPos, platSpeed));
+            Debug.Log("Executed");
+        }
+        else if ((Vector2)transform.position == startPos && buttonClicked)
+        {
+            Debug.Log("In else if");
+            StartCoroutine(LerpPosition(gameObject, endPos, platSpeed));
+            Debug.Log("Executed1");
+        }
+    }
+
+    IEnumerator LerpPosition(GameObject platform, Vector2 targetPosition, float speed)
+    {
+        float time = 0f;
+        Vector2 startPosition = platform.transform.position;
+
+        while ((Vector2)platform.transform.position != targetPosition)
+        {
+            Debug.Log("In while");
+            platform.transform.position = Vector2.Lerp(startPosition, targetPosition, time / speed);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        platform.transform.position = targetPosition;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.gameObject.transform.SetParent(gameObject.transform, true);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        collision.gameObject.transform.parent = null;
+    }
+
+    public bool ButtonClicked()
+    {
+        buttonClicked = true;
+        Debug.Log("clicked");
+        return buttonClicked;
+    }
+
+    public void Activate()
+    {
+        ButtonClicked();
+        Debug.Log("Activated");
+    }
+
+    //Draws lines between gizoms to show path
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(objStartPos.transform.position, objEndPos.transform.position);
+    }
+}
